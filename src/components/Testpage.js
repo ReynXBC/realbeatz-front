c3f5a453c00fff5aec4c3a9e8b9818b1fb34a61b
Reyn_XBC <cole.harris@my.uu.edu>
2022-12-07 17:02:38 -0600

    Friendpage added

--- src/components/Testpage.js
+++ src/components/Testpage.js
@@ -1,35 +1,10 @@
-import React, {useEffect, useState} from "react";
-import FriendPageCard from "./friends/FriendPageCard";
-import "../css/FriendPage.css"
+
 
 function TestPage() {
 
-    let [friendsList, setFriendsList] = useState([]);
-    useEffect(() => {
-
-        // fetch data from server
-        if (!friendsList.length) {
-            fetch("http://localhost:8080/api/test/friends")
-                .then(response => response.json())
-                .then(data => {
-                    console.log("fetching data...")
-                    setFriendsList(data);
-                })
-                .catch(error => console.log(error));
-        }
-    }, []);
-
     return (
-        <div id={'friendPageDiv'}>
-            {
-                friendsList.map(friend => {
-                    console.log(friend);
-                    return (
-                        <FriendPageCard friend={friend} />
-                    )
-                })
-            }
-        </div>
+        <>
+        </>
     );
 }
 
--- src/components/friends/FriendPage.js
+++ src/components/friends/FriendPage.js
@@ -1,1 +1,33 @@
-// Page displaying the profile of a friend
+import React, {useEffect, useState} from "react";
+import FriendPageCard from "./FriendPageCard";
+
+export default function FriendPage() {
+
+    let [userList, setUserList] = useState([]);
+    useEffect(() => {
+
+        // fetch data from server
+        if (!userList.length) {
+            fetch("http://localhost:8080/api/test/friends")
+                .then(response => response.json())
+                .then(data => {
+                    console.log("fetching data...")
+                    setUserList(data);
+                })
+                .catch(error => console.log(error));
+        }
+    }, []);
+
+    return (
+        <div id={'friendPageDiv'}>
+            {
+                userList.map(friend => {
+                    console.log(friend);
+                    return (
+                        <FriendPageCard friend={friend} />
+                    )
+                })
+            }
+        </div>
+    );
+}
