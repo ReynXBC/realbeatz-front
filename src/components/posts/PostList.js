8d89bce3998119d8142ef1e388f337c66d553d8b
Reyn_XBC <cole.harris@my.uu.edu>
2022-12-07 16:52:45 -0600

    Added post entry

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,11 +1,43 @@
 import Post from "./Post";
+import {useState} from "react";
 
-export default function postList({postList}) {
+export default function PostList({postList}) {
+
+    const [postInput, setPostInput] = useState();
+    console.log(postList);
+
+    const handlePost = (event) => {
+        event.preventDefault();
+        console.log(event.target);
+        // use fetch to call api
+    };
+
+
+    function handlePostInputChange(event) {
+        let value = event.target.value;
+        console.log(value);
+        setPostInput(value);
+    }
     return(
-        postList.map(postDetails => {
-            return (
-                <Post postDetails={postDetails}/>
-            )
-        })
+        <div className="PostDiv">
+            <div className="postEntryDiv">
+                <form id="postEntryForm" onSubmit={handlePost}>
+                    <input
+                        type="text" id="postCommentInput"
+                        name="postCommentInput" onChange={handlePostInputChange}
+                        value={postInput}/>
+                    <button className="btn">Post</button>
+                </form>
+            </div>
+            <div className="postListDiv">
+                {
+                    postList.map(postDetails => {
+                        return (
+                            <Post postDetails={postDetails}/>
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
