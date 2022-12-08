45ab1b62931e7224bb7a75511e3ef63029d3fdca
Reyn_XBC <cole.harris@my.uu.edu>
2022-12-07 21:38:50 -0600

    profile working on stuff

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,13 +3,15 @@ import {useState} from "react";
 export default function Profile() {
   const [userDetails, setUserDetails] = useState(
       {
-        username: "Juauauan",
-        firstName: "Jalin",
-        lastName: "Hyatt",
-        bio: "I have joined RealBeatz",
-        numOfFriends: 14,
-        joinDate: "11/11/2022",
-        profilePicture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN2vVVG2BOns1aicnn0wKmKn3aYtHCcBiysWlpz_c&s"
+          id: '',
+          username: "Juauauan",
+          firstName: "Jalin",
+          lastName: "Hyatt",
+          bio: "I have joined RealBeatz",
+          dob: "",
+          age: 0,
+          registrationDate: "11/11/2022",
+          profilePicture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN2vVVG2BOns1aicnn0wKmKn3aYtHCcBiysWlpz_c&s"
       }
   );
   return (
@@ -21,8 +23,7 @@ export default function Profile() {
           <p>Username: {userDetails.username}</p>
           <p>Name: {userDetails.firstName} {userDetails.lastName}</p>
           <p>Bio: {userDetails.bio}</p>
-          <p>Friends: {userDetails.numOfFriends}</p>
-          <p>Joined On {userDetails.joinDate}</p>
+          <p>Joined On {userDetails.registrationDate}</p>
 
         </div>
       </div>
