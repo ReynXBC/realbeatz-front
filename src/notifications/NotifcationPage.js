c5be62607cc74599118a2efcabcd78e1170fb98b
Reyn_XBC <cole.harris@my.uu.edu>
2022-12-08 00:47:35 -0600

    Notification request loop programmed

--- src/notifications/NotifcationPage.js
+++ src/notifications/NotifcationPage.js
@@ -1,8 +1,16 @@
 import NotificationCard from "./NotificationCard";
 
-export default function NotificationPage() {
+export default function NotificationPage(requests) {
   return (
-    <NotificationCard />
-  )
-
+    <div className={"friendPageDiv"}>
+      {
+        requests.map(request => {
+          console.log(request);
+          return (
+            <NotificationCard friend={request}/>
+          )
+        })
+      }
+    </div>
+  );
 }
